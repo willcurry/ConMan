b28e889553be3f77a12c31578890e3491bcbba7a
willcurry <willcurry@Wills-MBP.default>
2016-08-16 13:21:04 +0100

    ConMan tests

---
+++ ConManTests.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConManTests {
+    private ConMan conMan;
+    private InputStream stream;
+    private Writer writer;
+    private ConsoleUI consoleUI;
+
+    @Before
+    public void setUp() {
+        stream = new ByteArrayInputStream("Will".getBytes());
+        writer = new PrintWriter(System.out);
+        consoleUI = new ConsoleUI(stream, writer);
+        conMan = new ConMan(consoleUI);
+    }
+
+    @Test
+    public void searchingFirstNameReturnsContactWithThatFirstName() {
+        Contact contact = new Contact("Will", "Curry", "07555555555", "will@emailsite.com");
+        conMan.add(contact);
+        for (Contact will : conMan.allContacts) {
+            assertThat(will.firstName(), is("Will"));
+        }
+    }
+
+    @Test
+    public void searchingFirstNameReturnsAllContactsWithThatFirstName() {
+        Contact contact = new Contact("Will", "Curry", "07555555555", "will@emailsite.com");
+        Contact contact2 = new Contact("Will", "NotCurry", "07555555556", "will@emailsite.co.uk");
+        Contact contact3 = new Contact("Billy", "Smith", "07555555556", "billy@emailsite.com");
+        conMan.add(contact);
+        conMan.add(contact2);
+        conMan.add(contact3);
+        for (Contact will : conMan.searchContacts()) {
+            assertThat(will.firstName(), is("Will"));
+        }
+    }
+}
--- ConsoleUITests.java
+++ ConsoleUITests.java
@@ -17,27 +17,9 @@ public class ConsoleUITests {
 
     @Before
     public void setUp() {
-        InputStream stream = new ByteArrayInputStream("Will".getBytes());
-        Writer writer = new PrintWriter(System.out);
-        ConsoleUI consoleUI = new ConsoleUI(stream, writer);
-        ConMan conMan = new ConMan(consoleUI);
-    }
-
-    @Test
-    public void searchingFirstNameReturnsContactWithThatFirstName() {
-        Contact contact = new Contact("Will", "Curry", "07555555555", "will@emailsite.com");
-        for (Contact will : conMan.allContacts) {
-            assertThat(will.firstName(), is("Will"));
-        }
-    }
-
-    @Test
-    public void searchingFirstNameReturnsAllContactsWithThatFirstName() {
-        Contact contact = new Contact("Will", "Curry", "07555555555", "will@emailsite.com");
-        Contact contact2 = new Contact("Will", "NotCurry", "07555555556", "will@emailsite.co.uk");
-        Contact contact3 = new Contact("Billy", "Smith", "07555555556", "billy@emailsite.com");
-        for (Contact will : conMan.searchContacts()) {
-            assertThat(will.firstName(), is("Will"));
-        }
+        stream = new ByteArrayInputStream("Will".getBytes());
+        writer = new PrintWriter(System.out);
+        consoleUI = new ConsoleUI(stream, writer);
+        conMan = new ConMan(consoleUI);
     }
 }
